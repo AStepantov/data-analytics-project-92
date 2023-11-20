6971696a70491ff92abc86f06cf496225f37283b
AStepantov <142100442+AStepantov@users.noreply.github.com>
2023-11-20 20:51:28 +0300

    Add files via upload
    ход решения:
    1. соединил три таблицы через left join
    from sales s 
    left join products p 
    on s.product_id=p.product_id
    left join employees e 
    on s.sales_person_id = e.employee_id 
    2.   через concat объединил first_name и last_name присвоил alias  name
    3. количество операции посчитал через функцию count 
    4.  сумма выручки по продавцу рассчитал через функцию round(sum(s.quantity*p.price),0) и округлил до целого
    5. сгруппировал по полю name
    6. отфильтровал income desc
    7. вывел первые 10 значений через функцию limit

---
+++ top_10_total_income.sql
@@ -0,0 +1,12 @@
+select 
+concat(e.first_name,'', e.last_name) as name,
+count(s.product_id) as operations,
+round(sum(s.quantity*p.price),0) as income
+from sales s 
+left join products p 
+on S.product_id=P.product_id
+left join employees e 
+on s.sales_person_id = e.employee_id 
+group by e.first_name, e.last_name
+order by income desc 
+limit 10
