9760908b151dabc1dcff500b7d990590d4c486ad
AStepantov <142100442+AStepantov@users.noreply.github.com>
2023-11-27 21:43:38 +0300

    Update age_groups_Script.sql

--- age_groups_Script.sql
+++ age_groups_Script.sql
@@ -1,11 +1,12 @@
 select
 case 
 	when c.age >16 and c.age <=25 then '16-25'
-	when c.age >26 and c.age <=40 then '26-40'
+	when c.age >=26 and c.age <=40 then '26-40'
 	else '40+'
 end as age_category, 
 count (c.customer_id)
 from customers c  
 group by age_category
 order by age_category
+
 
