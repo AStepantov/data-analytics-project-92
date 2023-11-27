ae25e9a8d7861269a4a852ea48336caa3862e7fb
AStepantov <142100442+AStepantov@users.noreply.github.com>
2023-11-27 21:33:41 +0300

    Update age_groups_Script.sql
    был вложен файл с неверным запросом

--- age_groups_Script.sql
+++ age_groups_Script.sql
@@ -1,11 +1,11 @@
 select
-extract (year from s.sale_date )||'-'|| extract (month from s.sale_date ) as date,
-count(distinct c.first_name||'_'||c.last_name||'_'||c.age) as total_customer,
-round(sum(s.quantity*p.price),0) as income
-from sales s 
-left join customers c
-on s.customer_id =c.customer_id 
-left join products p 
-on  s.product_id = p.product_id
-group by s.sale_date
-order by s.sale_date
+case 
+	when c.age >16 and c.age <=25 then '16-25'
+	when c.age >26 and c.age <=40 then '26-40'
+	else '40+'
+end as age_category, 
+count (c.customer_id)
+from customers c  
+group by age_category
+order by age_category
+
