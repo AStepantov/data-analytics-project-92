893dceb55ce86576f2746316686b081b8c04df5c
AStepantov <142100442+AStepantov@users.noreply.github.com>
2023-11-29 22:19:25 +0300

    Update top_10_total_income.sql
    concat(e.first_name,'  ', e.last_name) as name, добавил пробел для разделения имени и фамилии

--- top_10_total_income.sql
+++ top_10_total_income.sql
@@ -1,5 +1,5 @@
 select 
-concat(e.first_name,'', e.last_name) as name,
+concat(e.first_name,' ', e.last_name) as name,
 count(s.product_id) as operations,
 round(sum(s.quantity*p.price),0) as income
 from sales s 
