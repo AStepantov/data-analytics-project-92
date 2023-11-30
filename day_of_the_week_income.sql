6edd0edbaefb04237ab15da5b7acc97ba05f3050
AStepantov <142100442+AStepantov@users.noreply.github.com>
2023-11-30 23:03:55 +0300

    Add files via upload

--- day_of_the_week_income.sql
+++ day_of_the_week_income.sql
@@ -11,15 +11,15 @@ on s.product_id = p.product_id
 group by e.first_name,e.last_name,day_num
 )
 select 
-t1.name,
+t1. name,
 case 
-	when t1.day_num = '1' then 'monday '
-	when t1.day_num = '2' then 'tuesday '
-	when t1.day_num = '3' then 'wednesday '
+	when t1.day_num = '1' then 'monday   '
+	when t1.day_num = '2' then 'tuesday  '
+	when t1.day_num = '3' then 'wednesday'
 	when t1.day_num = '4' then 'thursday '
-	when t1.day_num = '5' then 'friday '
+	when t1.day_num = '5' then 'friday   '
 	when t1.day_num = '6' then 'saturday '
-	when t1.day_num = '7' then 'sunday '
+	when t1.day_num = '7' then 'sunday   '
 	end as weekday,
 t1.income
 from t1
