b3083adc6e80ba06f20e04f2f0cfb6f4010459ef
AStepantov <142100442+AStepantov@users.noreply.github.com>
2023-11-30 22:34:21 +0300

    Add files via upload

--- day_of_the_week_income.sql
+++ day_of_the_week_income.sql
@@ -1,27 +1,26 @@
-with t1 as(
+with t1 as (
 select 
-concat(e.first_name,' ', e.last_name) as name,
-to_char(s.sale_date,'d') as day_num,
-sum(s.quantity*p.price) as income
-from sales as s  
-left join employees as e 
-on e.employee_id = s.sales_person_id  
-left join products as p 
-on s.product_id = p.product_id
+concat(e.first_name,' ',e.last_name) as name,
+extract (isodow from s.sale_date) as day_num,
+floor(sum(s.quantity*p.price)) as income
+from sales s 
+left join employees e 
+on s.sales_person_id = e.employee_id 
+left join products p 
+on s.product_id = p.product_id 
 group by e.first_name,e.last_name,day_num
-having sum(s.quantity*p.price) is not null
 )
 select 
-t1.name,
+t1. name,
 case 
 	when t1.day_num = '1' then 'monday'
-        when t1.day_num = '2' then 'tuesday'
-        when t1.day_num = '3' then 'wednesday'
-        when t1.day_num = '4' then 'thursday'
-        when t1.day_num = '5' then 'friday'
-        when t1.day_num = '6' then 'saturday'
-        when t1.day_num = '7' then 'sunday'
-end as weekday,
-round(income) as income
+	when t1.day_num = '2' then 'tuesday'
+	when t1.day_num = '3' then 'wednesday'
+	when t1.day_num = '4' then 'thursday'
+	when t1.day_num = '5' then 'friday'
+	when t1.day_num = '6' then 'saturday'
+	when t1.day_num = '7' then 'sunday'
+	end as weekday,
+t1.income
 from t1
-order by t1.day_num,t1.name;
+order by t1.day_num, t1.name
